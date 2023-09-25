2934a7308de4424823cee82ef3ea372371002912
Matt Tucker <arcanemahuloq@gmail.com>
2023-09-24 19:44:26 -0500

    Finished EX 2

---
+++ Excercise-2/ex.js
@@ -0,0 +1,7 @@
+// let studentName: string = "Matt";
+// let totalCourses: number = 3;
+// console.log(`${studentName} is taking ${totalCourses} courses`);
+var subjects = [];
+var student = [];
+student = ["Matt", 34];
+console.log(student);
---
+++ Excercise-2/ex.ts
@@ -0,0 +1,12 @@
+// let studentName: string = "Matt";
+
+// let totalCourses: number = 3;
+
+// console.log(`${studentName} is taking ${totalCourses} courses`);
+
+let subjects: string[] = [];
+
+let student: (string | number)[] = [];
+
+student = ["Matt", 34];
+console.log(student);
